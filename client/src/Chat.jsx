e28af2a265abc2e9d0eb1c3d5fac2f1170afe65e
Mekkee <mekke@live.se>
2020-08-19 16:47:53 +0200

    Updates: added subscription, live chat, webpack ModuleFederation for code sharing between apps

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,17 +3,28 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
+  useSubscription,
   gql,
+  useMutation,
 } from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { Container, Row, Col, FormInput, Button } from 'shards-react'
+
+const link = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true,
+  },
+})
 
 const client = new ApolloClient({
+  link,
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 })
 
 const GET_MESSAGES = gql`
-  query {
+  subscription {
     messages {
       id
       content
@@ -22,21 +33,124 @@ const GET_MESSAGES = gql`
   }
 `
 
-const Messages = ({}) => {
-  const { data } = useQuery(GET_MESSAGES)
+const POST_MESSAGES = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`
+
+const Messages = ({ user }) => {
+  const { data } = useSubscription(GET_MESSAGES)
 
   if (!data) {
     return null
   }
 
-  return JSON.stringify(data)
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            padding: '10px 0 10px 0',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                width: 50,
+                height: 50,
+                marginRight: '4px',
+                border: '2px solid #e5e6eb',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '20px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? '#000' : '#e5e6eb',
+              color: user === messageUser ? '#fff' : '#000',
+              padding: '20px',
+              borderRadius: '20px',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  )
 }
 
 const Chat = () => {
+  const [state, setState] = React.useState({
+    user: 'Arre',
+    content: '',
+  })
+
+  const [postMessage] = useMutation(POST_MESSAGES)
+
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({
+        variables: state,
+      })
+    }
+    setState({
+      ...state,
+      content: '',
+    })
+  }
+
   return (
-    <div>
-      <Messages user='Arre' />
-    </div>
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label='User'
+            value={state.user}
+            onChange={(event) =>
+              setState({
+                ...state,
+                user: event.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label='Content'
+            value={state.content}
+            onChange={(event) =>
+              setState({
+                ...state,
+                content: event.target.value,
+              })
+            }
+            onKeyUp={(event) => {
+              if (event.keyCode === 13) {
+                onSend()
+              }
+            }}
+          />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()} style={{ width: '100%' }}>
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer, PubSub } = require('graphql-yoga')
 
 const messages = []
 
@@ -8,15 +8,19 @@ const typeDefs = `
         user: String!
         content: String!
     }
-
     type Query {
         messages: [Message!]
     }
-
     type Mutation {
         postMessage(user: String!, content: String!): ID!
     }
+    type Subscription {
+      messages: [Message!]
+    }
 `
+
+const subscribers = []
+const onMessagesUpdates = (fn) => subscribers.push(fn)
 
 const resolvers = {
   Query: {
@@ -30,12 +34,24 @@ const resolvers = {
         user,
         content,
       })
+      subscribers.forEach((fn) => fn())
       return id
+    },
+  },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15)
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }))
+        setTimeout(() => pubsub.publish(channel, { messages }), 0)
+        return pubsub.asyncIterator(channel)
+      },
     },
   },
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers })
+const pubsub = new PubSub()
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } })
 server.start(({ port }) => {
   console.log(`Server on localhost:${port}/`)
 })
